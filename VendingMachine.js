2335d70e442a2da1dcdd827eb453bf19ee063200
Michael <mpiastucki@gmail.com>
2020-09-29 20:57:37 +0900

    Kenny/Michael pairwork; initial commit to Michael repo. js files simply cut/pasted into fresh clone because git push was not working

--- VendingMachine.js
+++ VendingMachine.js
@@ -1,6 +1,102 @@
 // your class here
+
+class VendingMachine {
+  constructor() {
+    this.balance = 0;
+    this.till = { 10: 10, 50: 10, 100: 10, 500: 10 };
+    this.drinks = {
+      A: ["cola", "pepsi", "fanta", "sprite"],
+      B: ["monster", "redbull", "rockstar", "charge"],
+      C: ["kirin", "sapporo", "asahi", "yebisu"],
+      D: ["gin", "vodka", "whiskey", "tequila"],
+    };
+    this.price = {
+      cola: 100,
+      pepsi: 100,
+      fanta: 100,
+      sprite: 100,
+      monster: 170,
+      redbull: 170,
+      rockstar: 190,
+      charge: 210,
+      kirin: 230,
+      sapporo: 250,
+      asahi: 10,
+      yebisu: 270,
+      gin: 530,
+      vodka: 470,
+      whiskey: 410,
+      tequila: 420,
+    };
+    this.inventory = {
+      cola: 10,
+      pepsi: 10,
+      fanta: 10,
+      sprite: 10,
+      monster: 10,
+      redbull: 10,
+      rockstar: 10,
+      charge: 10,
+      kirin: 10,
+      sapporo: 10,
+      asahi: 10,
+      yebisu: 10,
+      gin: 10,
+      vodka: 10,
+      whiskey: 10,
+      tequila: 10,
+    };
+  }
+  insertCoin(coin) {
+    for (const property in this.till) {
+      if (coin == property) {
+        this.till[property] += 1;
+        this.balance += coin;
+      }
+    }
+  }
+
+  selectRow(rowNumber) {
+    this.row = rowNumber - 1;
+    console.log(this.row);
+    return this.row;
+  }
+
+  selectColumn(columnNumber) {
+    this.column = columnNumber;
+    if (this.row !== undefined) {
+      let product = this.drinks[this.column][this.row];
+      let price = this.price[product];
+      if (this.balance >= price) {
+        console.log(`Row:${this.row}; Column:${this.column}`);
+        console.log(`Here is your ${product}`);
+        this.balance -= price; //reduce balance
+        this.inventory[this.drinks[this.column][this.row]] -= 1; //reduce inventory
+        return product;
+      } else {
+        return "insufficient balance!";
+      }
+    }
+    console.log(`you selected ${this.column}`);
+  }
+
+  changeReturn() {
+    let coinlist = [10, 50, 100, 500];
+    for (let i = coinlist.length - 1; i >= 0; i--) {
+      let coinName = coinlist[i];
+      if (this.balance >= coinlist[i]) {
+        this.till[coinName] -= Math.floor(this.balance / coinName);
+        this.balance -= Math.floor(this.balance / coinName) * coinName;
+      }
+    }
+    console.log(this.till);
+  }
+}
+
 /*
   >>> Don't forget to use module.exports!
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+
+module.exports = VendingMachine;
--- test/test.js
+++ test/test.js
@@ -2,6 +2,82 @@ const VendingMachine = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
+  it("should have a method for insertCoin", () => {
+    const machine = new VendingMachine();
+    expect(typeof machine.insertCoin).to.equal("function");
+  });
+
+  it("should have a beginning balance of 0 and till should be initialized to 10 for all coins", () => {
+    const machine = new VendingMachine();
+    expect(machine.balance).to.equal(0);
+    expect(machine.till[10]).to.equal(10);
+    expect(machine.till[50]).to.equal(10);
+    expect(machine.till[100]).to.equal(10);
+    expect(machine.till[500]).to.equal(10);
+  });
+
+  it("should have a method for selecting a row", () => {
+    const machine = new VendingMachine();
+    expect(typeof machine.selectRow).to.equal("function");
+  });
+
+  it("should return the correct row using selectRow", () => {
+    const machine = new VendingMachine();
+    const row = machine.selectRow(2);
+    expect(row).to.equal(1);
+  });
+
+  it("should have a method for selecting a column", () => {
+    const machine = new VendingMachine();
+    expect(typeof machine.selectColumn).to.equal("function");
+  });
+
+  it("should return the correct column using selectColumn", () => {
+    const machine = new VendingMachine();
+    machine.selectColumn("A");
+    expect(machine.column).to.equal("A");
+  });
+
+  it("should return correct drink and correct balance if the balance is sufficient", () => {
+    const machine = new VendingMachine();
+    machine.insertCoin(100);
+    machine.selectRow(1);
+    expect(machine.selectColumn("A")).to.equal("cola");
+    expect(machine.balance).to.equal(0);
+    expect(machine.inventory["cola"]).to.equal(9);
+  });
+
+  it("should have the correct till after transaction and changeReturn", () => {
+    const machine = new VendingMachine();
+    machine.insertCoin(500);
+    machine.selectRow(3);
+    machine.selectColumn("B");
+    machine.changeReturn();
+    let correctTill =
+      machine.till[10] === 9 &&
+      machine.till[50] === 10 &&
+      machine.till[100] === 7 &&
+      machine.till[500] === 11;
+    expect(correctTill).to.equal(true);
+    expect(machine.balance).to.equal(0);
+  });
+
+  it("should reset the balance to zero using change return", () => {
+    const machine = new VendingMachine();
+    machine.insertCoin(500);
+    machine.changeReturn();
+    expect(machine.balance).to.equal(0);
+  });
+
+  it("should have correct till balance after transaction", () => {
+    const machine = new VendingMachine();
+
+    machine.insertCoin(100);
+    machine.insertCoin(100);
+    machine.changeReturn();
+    expect(machine.till[100]).to.equal(10);
+  });
+
   it("should accept valid coins", () => {
     // Setup
     const machine = new VendingMachine();
@@ -11,11 +87,23 @@ describe("vending machine", () => {
 
     // Assert
     expect(machine.till).to.deep.equal({
-      10: 0,
-      50: 0,
-      100: 0,
-      500: 1,
+      10: 10,
+      50: 10,
+      100: 10,
+      500: 11,
     });
     expect(machine.balance).to.equal(500); // Use an ES6 getter
   });
 });
+
+/* 
+1.  _Given_ that the balance is zero, _when_ a coin is inserted, _then_ the balance should rise _and_ types of coins should be stored
+1.  _Given_ that no row is selected, _when_ a row is selected the letter should be saved and printed to the console
+1.  _Given_ that a row is selected, _when_ there is sufficient balance and inventory and a column is selected
+    1.  _then_ the row and column should be logged to the console
+    1.  _and_ a message should be logged stating "Here is your [item name]"
+    1.  _and_ the item inventory should decrease by 1
+    1.  _and_ the correct change should be returned (log type and number of coins to console)
+1.  _Given_ that a row and column are selected, _when_ there is no inventory at that column, _then_ an error message should be logged.
+1.  _Given_ that a row and column are selected, _when_ the balance is insufficient to purchase the selected item, _then_ an error message should be printed
+ */
